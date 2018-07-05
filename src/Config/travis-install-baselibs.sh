240acd3291abc772423374e9309847031c8594df
Matthew Thompson <matthew.thompson@nasa.gov>
2018-07-05 14:38:07 -0400

    Add explicit --version commands

--- src/Config/travis-install-baselibs.sh
+++ src/Config/travis-install-baselibs.sh
@@ -15,7 +15,9 @@ case "$BASELIBS_VERSION" in
    if [ ! -d "${HOME}/local/Baselibs/${BASELIBS_VERSION}/${os}/bin" ]
    then
       ALLDIRS='jpeg zlib szlib curl hdf4 hdf5 h5edit netcdf netcdf-fortran udunits2 nccmp esmf'
-      env
+      ${FC} --version
+      ${CC} --version
+      ${CXX} --version
       mkdir -p ${HOME}/Baselibs/src/ && cd ${HOME}/Baselibs/src/
       wget --no-check-certificate https://www.dropbox.com/s/fxbz31o82ihzfvg/ESMA-Baselibs-4.0.11.tar.gz
       tar xzf ESMA-Baselibs-4.0.11.tar.gz
