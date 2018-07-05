8b99dcf5a96b93c76da9ddf015a1bf309064ad8a
Matthew Thompson <matthew.thompson@nasa.gov>
2018-07-05 12:41:34 -0400

    Try and build baselibs now

---
+++ src/Config/travis-install-baselibs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# source: mpi4py
+# https://github.com/mpi4py/mpi4py/blob/master/conf/travis/install-mpi.sh
+
+set -e
+
+BASELIBS_VERSION="$1"
+MPISTACK="$2"
+COMPILER="$3"
+os=`uname`
+
+
+case "$BASELIBS_VERSION" in
+4.0.11)
+   if [ ! -d "${HOME}/local/Baselibs/${BASELIBS_VERSION}/${os}/bin" ]
+   then
+      ALLDIRS='jpeg zlib szlib curl hdf4 hdf5 h5edit netcdf netcdf-fortran udunits2 nccmp esmf'
+      mkdir -p ${HOME}/Baselibs/src/ && cd ${HOME}/Baselibs/src/
+      wget --no-check-certificate https://www.dropbox.com/s/fxbz31o82ihzfvg/ESMA-Baselibs-4.0.11.tar.gz
+      tar xjf ESMA-Baselibs-4.0.11.tar.gz
+      cd ESMA-Baselibs-4.0.11/src
+      make -j2 install ESMF_COMM=$MPISTACK ESMF_COMPILER=$COMPILER --prefix=${HOME}/local/Baselibs/${BASELIBS_VERSION}/${os} ALLDIRS=$ALLDIRS
+      cd ${HOME}
+      exit 0
+   else
+      echo "Using cached ${BASELIBS_VERSION} directory";
+      exit 0
+   fi
+   ;;
+*)
+   echo "Unknown MPI implementation: $BASELIBS_VERSION"
+   exit 1
+   ;;
+esac
